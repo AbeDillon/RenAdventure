74d1c63455b7b2597566a14feb3503dde6a5a7ec
Sean Whitten <SWhitten@SWhitten-LT1.Austin.Q2Technology.com>
2013-03-26 16:28:48 -0500

    added a crap load of changes to the authoring stuff.

--- room_builder.py
+++ room_builder.py
@@ -1,105 +1,89 @@
 __author__ = 'Andy Yeager', 'Sean Whitten'
 
+import validator
 import textwrap
+import makePortals
+import containerMaker
 import engine
 
 # Room builder
 
-class BuildRoom(object, player):
-    
-    '''This is a Room builder "module"  For players to build out rooms as they like.
-    Rooms must fit all room requirements for proper game play.
-    Follows structure of our Room Object
-    
-    Attributes:
-    - Player (Tag of name of player creating room) pass in likely later.
-    - Long Description
-    - Short Description
-    
-    
-    Contains:
-    - Portals
-    - Containers
-    - Items
-    '''
-    
-    def __init__(self):
-        
-        print textwrap.fill('In this module you will be building a "room or area" to your liking with some limits of course.  We will walk you through the process building '
-                            'and populating the room with things like Portals, Containers, Items, and even some other cool stuff.  So lets get started.', width = 150).strip()
-        print ''
-        self.lng_desc = ''
-        #print self.desc,'\n'
-        self.portals = {}
-        #print self.portals, '\n'
-        self.containers = {}
-        #print self.containers, '\n'
-        self.items = {}
-        #print self.items, '\n'
-        self.make_description(desc)
-    
-    def make_description():
-       
-        print ""
-        print textwrap.fill ('Tell us you what you would like the user to see as they enter your room for the first time.  This will be your "long description".', width = 150).strip()
+'''
+This is a Room builder "module"  For players to build out rooms as they like.
+Rooms must fit all room requirements for proper game play.
+Follows structure of our Room Object
 
-       
-       #  confirm loop for User description
-       
-        confirm = False
-        while confirm is False:
-           lng_desc = raw_input("\n>  ")
-           
-           #  Filter out unwanted room descriptions and other stuff
-           if lng_desc == "" or usr_desc == " ":  # no Empty Strings
-               lng_desc = raw_input('Description cannot be empty.  Try again.\n>  ')
-                              
-           else:
-               print '\n',lng_desc 
-               print ''
-               ans = raw_input('Is this the room description you want?\n>  ').lower()
-               if ans == 'yes' or ans == 'y':
-                   confirm = True 
-               else:
-                   lng_desc = raw_input("Enter your description.\n>  ")
-               
-        desc = lng_desc
-        self.make_Portals()
-    
-    def make_Portals():  
-        ##### ********* Is 1 portal per room going to be predefined because the player just came through a door? I left it as making 6 exits thus we have a free floating room.
-        portals = []
-        directions = ['north', 'south', 'east', 'west', 'up', 'down']
-        print ''
-        print textwrap.fill('Portals also known as exits from your room need to be defined.  Do you want to define any exits?',  width= 100).strip()
-        print ''
-        ans = raw_input('>  ').lower()
-        print ""
+Attributes:
+- Player (Tag of name of player creating room) pass in likely later.
+- Long Description
+- Short Description
+
+
+Contains:
+- Portals
+- Containers
+- Items
+'''
+
+global_portals = {'bob', 'juan'}
+room_portals = {}
+
+
+
+print textwrap.fill('In this module you will be building a "room" or "area" to your liking with some limits of course.  We will walk you through the process of building '
+                    'and populating the room with things like Portals, Containers, Items, and even some other cool stuff.  So lets get started.', width=100).strip()
+
+def make_description():
+
+    print ""
+    print textwrap.fill ('Tell us you what you would like the user to see as they enter your room for the first time.  This will be your "long description".', width=100).strip()
+
+
+   #  confirm loop for User description
+    confirm = False
+    while confirm is False:
+        desc = raw_input("\n>  ")
+
+       #  Filter out unwanted room descriptions and other stuff
+        if len(desc) > 0:
+            print '\n', desc
+            print '\nIs this the room description you want?'
+            ans = raw_input('\n>  ').lower()
+            if ans == 'yes' or ans == 'y':
+                confirm = True
+            else:
+                print 'Enter your description.'
+
+    #  make_Portals()
+    #  Ask if they want to create doors
+    print ''
+    print textwrap.fill('Portals, also known as exits, need to be defined.  Do you want to create any portals?',  width=100).strip()
+    right_ans = False
+    while right_ans == False:
+        ans = raw_input('\n>  ').lower().strip()
+        if  ans != 'no' and ans != 'n' and ans != 'yes' and ans != 'y':
+            print "Please answer Yes or No."
         # Check user answer and continue as they desire
-        if ans != 'yes' or ans != 'y':
-            # Go to next function
-            pass
-        else: # how many
-            qty = raw_input('How many would you like to create?\n>  ')
-            print ''
-        while qty > 0 : # Make as many doors as player specified
-            
-            # Door Name create dictionary for door
-            name = raw_input('What would you like to call this door?\n>  ')
-            print ""
-            # Verification of door name has to happen here.
-            #if != Global list/dict of all portals ever created  (Check name function???  used to check all items from a list/dict passed in?)
-                #deny creation
-            #else:
-            portal = {name:qty} # Make dictionary for this door to be added to later Name is dictionary key with door number for room as value.
-            
-            
-       
-         
-    
-    
-       
-        
-    
-            
-BuildRoom()      
+        elif ans == 'no' or ans == 'n':
+            right_ans = True  #  need to send to next Method later when defined pass for now.
+        elif ans == 'yes' or ans == 'y':  # anything other than yes or y
+            makePortals.makePortals()
+
+    #  containerCreation()
+    print ''
+    print textwrap.fill('Containers can be placed in your room to hold various items.  Do you want to create any containers?',  width=100).strip()
+    right_ans = False
+    while right_ans == False:
+        ans = raw_input('\n>  ').lower().strip()
+        if  ans != 'no' and ans != 'n' and ans != 'yes' and ans != 'y':
+            print "Please answer Yes or No."
+        # Check user answer and continue as they desire
+        elif ans == 'no' or ans == 'n':
+            right_ans = True  #  need to send to next Method later when defined pass for now.
+        elif ans == 'yes' or ans == 'y':  # anything other than yes or y
+            containerMaker.makeContainers()
+
+
+
+
+make_description()
