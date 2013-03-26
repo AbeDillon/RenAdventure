465adbd2f3d879564ec04ddf365d48a350eee466
eking <eking@q2ebanking.com>
2013-03-26 15:13:17 -0500

    updated scripts

--- engine.py
+++ engine.py
@@ -142,7 +142,7 @@ class NPC:
 def scrub(scripts):
     # Scrubs the verbs in the script to make sure they are valid, no sneaky code injection
     valid_verbs = ['take', 'open', 'go', 'drop', 'unlock', 'print_text', 'reveal']
-    
+
     for script in scripts.keys():
         for action in scripts[script]:
             verb = action[0]
@@ -170,7 +170,7 @@ def init_game():
 
     thread.start_new_thread(command_thread, ())
 
-def make_player(name, coords, affiliation):
+def make_player(name, coords = (0,0,1), affiliation = {}):
     global _Rooms
     global _Players
 
@@ -302,20 +302,21 @@ def do_command(player, command):
         script = "custom_script(room, player, object.scripts[verb])"
     else:
         script = verb + "(room, player, object, noun_string)"
-    
-    text, alt_text = eval(script)
-    
-    messages = [(player.name, text)]
 
-    if len(alt_text) > 0:
-        for alt_player in room.players.values():
-            if alt_player is not player:
-                messages.append((alt_player.name, alt_text))
-
-        if verb == 'go': # Player entered a new room pass messages to all players in the new room
-            room = _Rooms[player.coords]
+    if 'custom_script' in script:
+        messages = eval(script)
+    else:
+        text, alt_text = eval(script)
+        messages = [(player.name, text)]
+        if len(alt_text) > 0:
             for alt_player in room.players.values():
-                messages.append((alt_player.name, "%s has entered the room." % player.name))
+                if alt_player is not player:
+                    messages.append((alt_player.name, alt_text))
+
+            if verb == 'go': # Player entered a new room pass messages to all players in the new room
+                room = _Rooms[player.coords]
+                for alt_player in room.players.values():
+                    messages.append((alt_player.name, "%s has entered the room." % player.name))
     
     return messages
     
@@ -489,7 +490,7 @@ def get_valid_objects(player, room, verb):
     return valid_objects
 
 def get_all_objects(player, verb):
-    # Returns all valid objects in the game for a verb
+    # Returns all valid objects in the game for a verb and the room they are in
     global _ValidLookUp
     global _Rooms
     
@@ -499,29 +500,29 @@ def get_all_objects(player, verb):
     for room in _Rooms.values():
         if 'r' in flags:
             for item in room.items.values():
-                valid_objects.append(item)
+                valid_objects.append((room, item))
         
         if 'p' in flags:
             for portal in room.portals.values():
-                valid_objects.append(portal)
+                valid_objects.append((room, portal))
         
         if 'c' in flags:
             for container in room.containers.values():
-                valid_objects.append(container)
+                valid_objects.append((room, container))
     
     if 'i' in flags:
         for item in player.items.values():
-            valid_objects.append(item)
+            valid_objects.append((None, item))
     
-    for object in valid_objects:
+    for room, object in valid_objects:
         for attribute, value in enumerate(cull):
             if isinstance(object, Item):    # Items are the only object that have all attributes
                 if attribute == 'portable' and object.portable == value:
-                    valid_objects.remove(object)
+                    valid_objects.remove((room, object))
                     break
             
             if attribute == 'hidden' and object.hidden == value:
-                valid_objects.remove(object)
+                valid_objects.remove((room, object))
                 break
     
     return valid_objects
@@ -706,19 +707,28 @@ def bad_command(room, player, object, noun, script=False):
     return "That is not a valid command."
 ############# CUSTOM SCRIPT METHODS ##########
 def custom_script(room, player, script):
-    message = ''
-    for verb, noun in script:
+    messages = []
+    for verb, noun, delay in script:
         nouns = noun.split()
         valid_objects = get_all_objects(player, verb)
-        object = get_object(nouns, valid_objects)
-        
+
+        objects = []
+        for object_room, object in valid_objects:
+            objects.append(object)
+
+        object = get_object(nouns, objects)
+
         script = verb + "(room, player, object, noun, script=True)"
-        text = eval(script)
-        
-        if verb == 'print_text':
-            message += text
-    
-    return message
+        text, alt_text = eval(script)
+
+        for room, new_object in valid_objects: # Find the room the object is in
+            if object is new_object:
+                break
+
+        for room_player in room.players:
+            messages.append((room_player, alt_text))
+
+    return messages
         
 def print_text(room, player, object, noun, script=False):
     return noun
@@ -726,7 +736,13 @@ def print_text(room, player, object, noun, script=False):
 def reveal(room, player, object, noun, script=False):
     # Reveals a hidden object
     object.hidden = False
+    text = "A %s appears in the room." % object.name
+
+    return text, text
 
 def hide(room, player, object, noun, script=False):
     # Hides an object
     object.hidden = True
+    text = "The %s disappears from the room." % object.name
+
+    return text, text
