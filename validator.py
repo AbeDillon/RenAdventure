afc63c3afcefdf3d50f4d03e964d1c838775e150
Andy Yeager <ayeager@q2ebanking.com>
2013-03-27 15:37:43 -0500

    changed function names to better represent what they do.  Added a list of names that we can append to for all global names to validate against

--- validator.py
+++ validator.py
@@ -1,12 +1,19 @@
 
+names = ['door', 'sword', 'key', 'piano', 'chest']
 
-
-def validate_name(name, dictionary):  #  Function to validate a given name against a given dictionary for originality.
+def validate_name(name, list):  #  function to validate if name is in a given list
+    if name in list:  # If the name is in list it returns False
+        return False
+    elif name not in list:  #  If the name is not in the list returns True
+        return True
+    
+    
+def original_name(name, list):  #  Function to validate a given name against a given list/dictionary for originality.
     '''Names of things must be unique this will take a given name and validate it against a given list/dictionary '''
-    if name == '':
+    if len(name) <= 0:
         print 'Names must contain at least 1 character to be valid.'
         return False
-    if name in dictionary:
+    if name in list:
         print 'That name has already been used.'
         return False
     else:
