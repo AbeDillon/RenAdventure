5d68f5091d9cf172f94ed3b25c2943cbe925d2f0
Andy Yeager <ayeager@q2ebanking.com>
2013-03-27 17:16:31 -0500

    this needs complete edit throughout. incorrect function throughout etc...  Just copy of make container at the moment

---
+++ makeItems.py
@@ -0,0 +1,15 @@
+'Item maker'
+
+
+def initiateMaker():
+    
+    print textwrap.fill('You have entered a maker that builds (1) container(s) and (2) item(s).  Containers are objects that can open and close  '
+                        'be locked or unlocked', width = 100).strip()
+    
+
+def makeContainer():
+    pass
+
+def makeItem():
+    pass                
+
