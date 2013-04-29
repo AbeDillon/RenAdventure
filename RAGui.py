19fb5281a5e313229a398889d221f6f3dbea865e
AYeager78 <ayeager78@gmail.com>
2013-04-29 17:29:05 -0500

    This is the stripped down Queued version

--- RAGui.py
+++ RAGui.py
@@ -31,7 +31,6 @@ class RenConsole(QtGui.QDialog):
 
         # input area
         self.inputBox = QtGui.QLineEdit()
-        self.inputBox.setFocus()
         #self.inputBox.setPalette(self.pallette)
         #self.connect(self.inputBox, QtCore.SIGNAL("returnPressed()"), self.sendInput )
 
@@ -41,11 +40,7 @@ class RenConsole(QtGui.QDialog):
         layout.addWidget(self.inputBox)
         self.setLayout(layout)
 
-        self.inputBox.setFocus()
-        self.mainDisplay.append('\n'*15 + 'Welcome to the Ren Adventure!')
-        self.loggedIn = False
-        self.playerName = ""
-        self.password = ""
+
 
 #++++++++LOGIC OBJECTS+++++++++++++++++++++++++++++++
 class InThread(threading.Thread):
@@ -60,23 +55,85 @@ class OutThread(threading.Thread):
 
 
 def login():
+    """  Goes through the login process in the Gui Window """
+    #global _CMD_Queue
+
     global _In_Queue
+    global  _Out_Queue
+    loggedin = False
 
-    _In_Queue.put('Please enter your Player Name')
+    while not loggedin:
+        _In_Queue.put('Please enter your User Name. Or type "new" to create a new player')
+        playerName =  _Out_Queue.get()
+        if playerName in ["n", "N", "new", "New", "NEW"]:
+            # create a new player
+            break
+        _In_Queue.put('Please Enter Your Password')
+        password = _Out_Queue.get()
 
-    pass
+        loginLine= playerName, password
+        ports = connect_to_server(loginLine)
+
+        if ports in ['invalid', 'banned_player', 'affiliation_get']:
+            _In_Queue.put('That player/password is invalid or on the banned list.  Please try again.')
+        else:
+            pass
+
+
+
+    # while console.loggedIn == False:
+
+    #         # need to wait for response before prompting again
+    #     if console.playerName == 'new':
+    #         affDict = getAffiliation()
+    #     while console.password == "":
+    #         console.mainDisplay.append('Please enter your password.')
+    #         # wait for response before prompting again
+    #     # gather data and send to login
+    #     loginString = console.playerName, console.password
+    #     console.mainDisplay.append(loginString)
+    #     ports = connect_to_server(loginString)
+    #     if ports == 'invalid' or ports == 'banned_player':
+    #         #  Not valid login reset variables and start over
+    #         console.mainDisplay.append('Your player/password was invalid, banned, or not found try again.')
+    #         console.playerName = ""
+    #         console.password = ""
+
+def connect_to_server(line):
+    """
+
+    """
+    global _Server_Host
+    global _Login_Port
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_sock = ssl.wrap_socket(sock, certfile = 'cert.pem')
+    ssl_sock.connect((_Server_Host, _Login_Port))
+
+    RAProtocol.sendMessage(line, ssl_sock)
+    logger.write_line('Hidden: Making connection with remote server')
+
+    message = RAProtocol.receiveMessage(ssl_sock)
+
+    ssl_sock.close()
+
+    return message
+
+
+
+
+
+
 
 def playerInput():
     #  Function receives player input from console.inputBox
     global _Out_Queue
 
     line = console.inputBox.displayText()
-    _Out_Queue.put(line)
     _In_Queue.put(line)
+    _Out_Queue.put(line)
     console.inputBox.clear()
 
-def playerInputLogin():
-    pass
 
 def printMessage():
     #  Function to print message received from Server
@@ -90,6 +147,18 @@ def printMessage():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
 
     app = QtGui.QApplication(sys.argv)
@@ -97,8 +166,8 @@ if __name__ == '__main__':
 
     timer = QtCore.QTimer(console)
     timer.start(35)
-    console.mainDisplay.append('\n'*15+'Welcome to the Ren Adventure.')
-
+    console.mainDisplay.append('\n'*15+'Welcome to the Ren Adventure!!')
+    login()
     timer.timeout.connect(printMessage)
 
     console.inputBox.returnPressed.connect(playerInput)
